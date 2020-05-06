5c5f076ae78650dd9954dac3e33342d9ddc86832
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-07 01:10:19 +0530

    typography

--- src/Contents/About.js
+++ src/Contents/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
 <section className="section">
     <div className="container center">
-        <h1>WHO ARE YOU ?</h1>
+        <h1>WHO ARE WE ?</h1>
         <p>An organization aimed at providing guidance in sustaining businesses through professionals who are best in the industry. </p>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
     </div>
