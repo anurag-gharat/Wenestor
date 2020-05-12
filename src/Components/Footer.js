ee1bd0654acd20fa82cf256de9a006ff065c25d3
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-12 22:54:25 +0530

    footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,25 +6,31 @@ import {GrInstagram,GrTwitter,GrFacebookOption} from 'react-icons/gr'
 export default function Footer() {
     return (
             <footer className="center no-mar blue-grey darken-2 white-text">
-                    <div className="row no-mar">
-                        <div className="col l8 left-align">
+                    <div className="row  no-mar">
+                        <div className="col l12 s12 left-align">
                             <ul className="footer-link">
-                                <li><Link to="#">Home</Link></li>
-                                <li><Link to="#">Home</Link></li>
-                                <li><Link to="#">Home</Link></li>
-                                <li><Link to="#">Home</Link></li>
-                                <li><Link to="#">Home</Link></li>
+                                <li><Link className="white-text" to="#">Home</Link></li>
+                                <li><Link className="white-text" to="#">About</Link></li>
+                                <li><Link className="white-text" to="#">Policy</Link></li>
+                                <li><Link className="white-text" to="#">Login</Link></li>
+                                <li><Link className="white-text" to="#">Signup</Link></li>
                             </ul>
                         </div>
-                        <div className="col l4 right-align">
-                            <ul className="footer-link">
-                                <li><GrFacebookOption className="display-4"/></li>
-                                <li><GrInstagram  className="display-4"/></li>
-                                <li><GrTwitter  className="display-4"/></li>
-                            </ul>
+                        <div className="divider"></div>
                         </div>
-                        
-                    </div>
+                        <div className="row m-t-10">
+                           <div className="col l6   s12 center-align m-t-10">
+                               <h6>WENESTER</h6>
+                               <p>An organization aimed at providing guidance in sustaining businesses through professionals who are best in the industry.</p>
+                            </div>
+                            <div className=" col l6 s12   social-links m-t-10  " >
+                         
+                            <GrFacebookOption className="display-4"/>
+                            <GrInstagram  className="display-4"/>
+                            <GrTwitter  className="display-4"/>
+                           
+                            </div> 
+                        </div>
                     <div className="row no-mar center blue-grey darken-4 white-text" >
                         <p>COPYRIGHT 2020 &copy;| ALL RIGHTS RESERVED</p>
                     </div>
